66c1581d0668f53b62f8ff0aa30488b3e89bfa5e
MarisHeinols <73535385+MarisHeinols@users.noreply.github.com>
2021-10-09 18:40:52 +0300

    Aded new project

--- src/components/ProjectFetch.js
+++ src/components/ProjectFetch.js
@@ -1,25 +1,41 @@
 import React from "react";
 
 const Projectfetch = (props) => {
-  const data = props.data;
-  return (
-    <div className='grid-3 py-3'>
-      {data.map((project) => {
-        return (
-          <a href={project.GitHub} style={{ color: "black" }}>
-            <ul className='card_content' key={project.id}>
-              <img className='project-icon' src={project.Img} alt=''></img>
-              <h2 className='text-dark'>{project.Name}</h2>
-              <h3>
-                Laungage:{" "}
-                <span className='text-primary'>{project.Language}</span>{" "}
-              </h3>
-              <h3 className='text-fade'>{project.Discrption}</h3>
-            </ul>
-          </a>
-        );
-      })}
-    </div>
-  );
+	const data = props.data;
+	return (
+		<div className="grid-4 py-3">
+			{data.map((project) => {
+				return (
+					<a
+						href={project.GitHub}
+						style={{ color: "black" }}
+					>
+						<ul
+							className="card_content"
+							key={project.id}
+						>
+							<img
+								className="project-icon"
+								src={project.Img}
+								alt=""
+							></img>
+							<h2 className="text-dark">
+								{project.Name}
+							</h2>
+							<h3>
+								Laungage:{" "}
+								<span className="text-primary">
+									{project.Language}
+								</span>{" "}
+							</h3>
+							<h3 className="text-fade">
+								{project.Discrption}
+							</h3>
+						</ul>
+					</a>
+				);
+			})}
+		</div>
+	);
 };
 export default Projectfetch;
--- src/components/data/projectData.js
+++ src/components/data/projectData.js
@@ -1,33 +1,48 @@
 import atajoLogo from "../graphics/images/atajo.png";
 import dice_gameLogo from "../graphics/images/dice_game.png";
 import collor_dungenLogo from "../graphics/images/collor_dungen.png";
+import W1 from "../graphics/images/W1.png";
+
 export const projectData = [
-  {
-    id: "1",
-    Name: "Atajo",
-    Language: "React Native",
-    Img: atajoLogo,
-    Discrption:
-      "Mouckup navigation app, that would help navigate the streets for users",
-    GitHub: "https://github.com/MarisHeinols/Atajo",
-  },
-  {
-    id: "2",
-    Name: "Dice game",
-    Language: "Java",
-    Img: dice_gameLogo,
-    Discrption: "Game where you must reach score before AI by rolling dice",
-    GitHub: "",
-  },
-  {
-    id: "3",
-    Name: "Collor Dungens",
-    Language: "Java",
-    Img: collor_dungenLogo,
-    Discrption:
-      "Adventure arcade game, games goal is to reach end of the dungen using collors as your friend",
-    GitHub: "https://github.com/MarisHeinols/SecCourseWork",
-  },
+	{
+		id: "1",
+		Name: "Atajo",
+		Language: "React Native",
+		Img: atajoLogo,
+		Discrption:
+			"Mouckup navigation app, that would help navigate the streets for users",
+		GitHub:
+			"https://github.com/MarisHeinols/Atajo",
+	},
+	{
+		id: "2",
+		Name: "Dice game",
+		Language: "Java",
+		Img: dice_gameLogo,
+		Discrption:
+			"Game where you must reach score before AI by rolling dice",
+		GitHub: "",
+	},
+	{
+		id: "3",
+		Name: "Collor Dungens",
+		Language: "Java",
+		Img: collor_dungenLogo,
+		Discrption:
+			"Adventure arcade game, games goal is to reach end of the dungen using collors as your friend",
+		GitHub:
+			"https://github.com/MarisHeinols/SecCourseWork",
+	},
+	{
+		id: "4",
+		Name: "Webshop",
+		Language: "React / Java",
+		Img: W1,
+		Discrption:
+			"Webshop created as a project in Accenture Java bootcamp",
+		GitHub:
+			"https://github.com/MarisHeinols/Webshop",
+	},
 ];
 
 export default projectData;
